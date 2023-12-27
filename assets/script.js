0d3078dbc938ded5039b77d88539da55efc73ce0
Kentana07 <151044422+Kentana07@users.noreply.github.com>
2023-12-27 15:37:16 +0100

    fait pas chez moi
    proto main

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,64 @@
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "C:/Users/Ken/Downloads/Java-script_open-main/Java-script_open-main/assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    image: "C:/Users/Ken/Downloads/Java-script_open-main/Java-script_open-main/assets/images/slideshow/slide2.jpg",
+    tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "C:/Users/Ken/Downloads/Java-script_open-main/Java-script_open-main/assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "C:/Users/Ken/Downloads/Java-script_open-main/Java-script_open-main/assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
 
-// Modèle d'objet de document sélections d'éléments
-const slideContainers = document.querySelectorAll(".banner-img");
-const dots = document.querySelectorAll(".dot");
-const nextButton = document.querySelector(".arrow_right");
-const prevButton = document.querySelector(".arrow_left");
+const banner = document.getElementById("banner");
+const imageContainer = banner.querySelector(".banner-img");
+const tagLineElement = banner.querySelector("#tagline");
+const dots = banner.querySelectorAll(".dot");
+const nextButton = banner.querySelector(".arrow_right");
+const prevButton = banner.querySelector(".arrow_left");
+
 let currentIndex = 0;
-let tagLine = document.querySelector("#tagline");
 
-// Définir la première diapositive comme active
-slideContainers[0].classList.add("active");
-dots[0].classList.add("dot_selected");
+// Set the initial image and tagline
+updateSlide();
 
-// Mettre à jour les points en fonction de la diapositive actuelle
+function updateSlide() {
+  imageContainer.src = slides[currentIndex].image;
+  tagLineElement.innerHTML = slides[currentIndex].tagLine;
+  updateDots();
+}
+
 function updateDots() {
   dots.forEach((dot, index) => {
     dot.classList.toggle("dot_selected", index === currentIndex);
   });
 }
 
-// Fonction pour afficher la diapositive suivante
 function showNextSlide() {
-  slideContainers[currentIndex].classList.remove("active");
-  dots[currentIndex].classList.remove("dot_selected");
-
   currentIndex = (currentIndex + 1) % slides.length;
-
-  slideContainers[currentIndex].classList.add("active");
-  dots[currentIndex].classList.add("dot_selected");
-
-  // Update tagline
-  tagLine.innerHTML = slides[currentIndex].tagLine;
+  updateSlide();
 }
 
-// Fonction pour afficher la diapositive précédente
 function showPrevSlide() {
-  slideContainers[currentIndex].classList.remove("active");
-  dots[currentIndex].classList.remove("dot_selected");
-
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-
-  slideContainers[currentIndex].classList.add("active");
-  dots[currentIndex].classList.add("dot_selected");
-
-  // Update tagline
-  tagLine.innerHTML = slides[currentIndex].tagLine;
+  updateSlide();
 }
 
-// Écouteurs d'événements pour les boutons suivant et précédent
+// Event listeners for navigation
+document.addEventListener("DOMContentLoaded", updateSlide); // Update slide on page load
 nextButton.addEventListener("click", showNextSlide);
 prevButton.addEventListener("click", showPrevSlide);
-
-// Affichage initial de la première diapositive
-updateDots();
-tagLine.innerHTML = slides[currentIndex].tagLine;
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    showNextSlide();
+  } else if (e.key === "ArrowLeft") {
+    showPrevSlide();
+  }
+});
